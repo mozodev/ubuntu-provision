36def25d7e11dd753449cfa909b88a589e0dcf6a
yeongtaek, Hong <mozodev@users.noreply.github.com>
2021-05-07 23:25:44 +0900

    php 프로비저닝 스크립트를 다듬습니다.

--- lang/php.sh
+++ lang/php.sh
@@ -4,51 +4,72 @@ if [ -f .env ]; then
   export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
 fi
 
-PHP_VERSION=${PHP_VERSION:-7.4}
+PHP_VERSION=${PHP_VERSION:-8.0}
 ALLOWED_PHP_VERSIONS=('5.6', '7.0','7.1', '7.2', '7.3', '7.4', '8.0')
+
+PROJECT_CODE=${PROJECT_CODE:-}
+PROJECT_ENV=${PROJECT_ENV:}
+[ ! -z $PROJECT_CODE ] && sudo mkdir -p /var/www/$PROJECT_CODE/web
+
+PHP_UPLOAD_MAX_SIZE=${PHP_UPLOAD_MAX_SIZE:-}
+PHP_UPLOAD_MAX_FILES=${PHP_UPLOAD_MAX_FILES:-}
+
 if [[ "${ALLOWED_PHP_VERSIONS[*]}" =~ "$PHP_VERSION" ]]; then
   echo "[php] install php $PHP_VERSION, apache2"
   yes | sudo add-apt-repository ppa:ondrej/php && sudo apt-get update
   sudo apt-get install -y -qq zip unzip apache2 php${PHP_VERSION}-{cli,gd,xml,curl,mbstring,zip,opcache,apcu,mysql,fpm,yaml}
   sudo a2enmod rewrite proxy_fcgi && sudo a2enconf php${PHP_VERSION}-fpm
   
-  echo "[php] set config"
-  cat <<EOF | sudo tee /etc/php/${PHP_VERSION}/cli/conf.d/php-dev.ini | sudo tee /etc/php/${PHP_VERSION}/fpm/conf.d/php-dev.ini
+  echo "[php] add timezone"
+  cat <<EOF | sudo tee /etc/php/${PHP_VERSION}/cli/conf.d/$PROJECT_CODE.ini | sudo tee /etc/php/${PHP_VERSION}/fpm/conf.d/$PROJECT_CODE.ini
 [PHP]
+date.timezone = Asia/Seoul
+post_max_size = 2048GB
+memory_limit = 512MB
+EOF
+
+  if [ ! -z $PHP_UPLOAD_MAX_SIZE ]; then
+    cat <<EOF | sudo tee /etc/php/${PHP_VERSION}/cli/conf.d/$PROJECT_CODE.ini | sudo tee /etc/php/${PHP_VERSION}/fpm/conf.d/$PROJECT_CODE.ini
+upload_max_filesize = $PHP_UPLOAD_MAX_SIZE
+EOF
+  fi
+
+  if [ ! -z $PHP_UPLOAD_MAX_FILES ]; then
+    cat <<EOF | sudo tee /etc/php/${PHP_VERSION}/cli/conf.d/$PROJECT_CODE.ini | sudo tee /etc/php/${PHP_VERSION}/fpm/conf.d/$PROJECT_CODE.ini
+max_file_uploads = $PHP_UPLOAD_MAX_FILES
+EOF
+  fi
+
+  if [ ! -z $PROJECT_ENV ] && [ "$PROJECT_ENV" == "dev" ]; then
+    cat <<EOF | sudo tee /etc/php/${PHP_VERSION}/cli/conf.d/$PROJECT_CODE.ini | sudo tee /etc/php/${PHP_VERSION}/fpm/conf.d/$PROJECT_CODE.ini
 max_execution_time = 0
 max_input_time = 0
 memory_limit = -1
-error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT
-display_startup_errors = On
-log_errors = On
-log_errors_max_len = 1024
-date.timezone = Asia/Seoul
-upload_max_filesize = 102400M
-post_max_size = 0
-max_file_uploads=100
 EOF
-  echo 'php_admin_value[error_log] = /vagrant/fpm-php.www.log' | sudo tee -a /etc/php/${PHP_VERSION}/fpm/pool.d/www.conf
+  fi
+
+  echo 'php_admin_value[error_log] = /var/www/$PROJECT_CODE/fpm-php.www.error.log' | sudo tee -a /etc/php/${PHP_VERSION}/fpm/pool.d/www.conf
 else
   echo "$PHP_VERSION not supported."
   exit 1
 fi
 
-APACHE_DOCROOT=${APACHE_DOCROOT:-}
-if [ ! -z $APACHE_DOCROOT ] && [ -d $APACHE_DOCROOT ]; then
-  echo "[apache2] add virtualhost"
-  cat <<EOF | sudo tee /etc/apache2/sites-available/vagrant.conf
+if [ ! -z $PROJECT_CODE ] && [ -d /var/www/$PROJECT_CODE/web ]; then
+  echo "[apache2] add virtualhost $PROJECT_CODE"
+  cat <<EOF | sudo tee /etc/apache2/sites-available/$PROJECT_CODE.conf
 <VirtualHost *:80>
   ServerAdmin $(git config user.email)
-  DocumentRoot $APACHE_DOCROOT
-  <Directory $APACHE_DOCROOT>
+  DocumentRoot /var/www/$PROJECT_CODE/web
+  <Directory /var/www/$PROJECT_CODE/web>
     Options Indexes FollowSymLinks
     AllowOverride All
     Require all granted
   </Directory>
 </VirtualHost>
 EOF
-  sudo a2dissite 000-default && sudo a2ensite vagrant && sudo service apache2 reload
+  sudo a2dissite 000-default && sudo a2ensite $PROJECT_CODE && sudo service apache2 reload
 fi
 
 echo "[php] install composer"
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+sudo chown -R $USER:$USER /usr/lobal/bin
