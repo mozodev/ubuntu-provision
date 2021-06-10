afc84c2d802da2417161bde707afe20bd841bdfd
yeongtaek, Hong <mozodev@users.noreply.github.com>
2021-06-10 16:34:43 +0900

    프로젝트 관련 코드를 정리합니다.

--- project.sh
+++ project.sh
@@ -6,13 +6,14 @@ fi
 
 PROJECT_GITREPO=${PROJECT_GITREPO:-}
 PROJECT_ROOT=${PROJECT_ROOT:-}
+PROJECT_USER=${PROJECT_USER:-}
 
 ssh-keyscan github.com >> $HOME/.ssh/known_hosts
 
 if [ ! -v $PROJECT_ROOT ] && [ ! -d $PROJECT_ROOT ]; then
-    echo create directory $PROJECT_ROOT and chown to $USER.
+    echo create directory $PROJECT_ROOT and chown to $PROJECT_USER.
     sudo mkdir -p $PROJECT_ROOT
-    sudo chown -R $USER:$USER $PROJECT_ROOT
+    sudo chown -R $PROJECT_USER:$PROJECT_USER $PROJECT_ROOT
 fi
 
 if [ ! -v $PROJECT_GITREPO ] && [ -d $PROJECT_ROOT ]; then
