5f02127f7415df69e4d93cb28520f9b40b8fa3e4
yeongtaek, Hong <mozodev@users.noreply.github.com>
2021-06-22 17:00:10 +0900

    yarn은 설치하지 않습니다.

--- lang/nodejs.sh
+++ lang/nodejs.sh
@@ -7,12 +7,7 @@ NODE_VERSION=${NODE_VERSION:-14}
 ALLOWED_NODE_VERSIONS=('10', '12', '13', '14', '15')
 if [[ "${ALLOWED_NODE_VERSIONS[*]}" =~ "$NODE_VERSION" ]]; then
   echo [nodejs] add repo for v$NODE_VERSION node, yarn
-  curl -fsSL https://deb.nodesource.com/setup_$NODE_VERSION.x | sudo bash
-  curl -fsSL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
-  echo [nodejs] install nodejs LTS v$NODE_VERSION and yarn
-  sudo apt-get update && sudo apt install -y nodejs yarn
+  curl -fsSL https://deb.nodesource.com/setup_$NODE_VERSION.x | sudo -E bash
 else
   echo "$NODE_VERSION not supported."
   exit 1
