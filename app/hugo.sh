f19978f586c52709ceb8356cdd96ebeb8e274d6e
yeongtaek, Hong <mozodev@users.noreply.github.com>
2021-07-03 12:45:20 +0900

    hugo snap 패키지로 설치합니다.

--- app/hugo.sh
+++ app/hugo.sh
@@ -4,9 +4,5 @@ if [ -f .env ]; then
     export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
 fi
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
-echo 'alias hs="hugo serve --bind=0.0.0.0"' >> ~/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-brew install hugo
+# https://gohugo.io/getting-started/installing/#snap-package
+sudo snap install hugo --channel=extended
