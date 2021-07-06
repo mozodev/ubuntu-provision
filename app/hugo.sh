6b3c0a47ebb0528c410a1cccebf8d69ec9573f8e
yeongtaek, Hong <mozodev@users.noreply.github.com>
2021-07-06 14:23:18 +0900

    hugo는 그냥 linuxbrew로 설치합니다.

--- app/hugo.sh
+++ app/hugo.sh
@@ -4,5 +4,9 @@ if [ -f .env ]; then
     export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
 fi
 
-# https://gohugo.io/getting-started/installing/#snap-package
-sudo snap install hugo --channel=extended
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
+echo 'alias hs="hugo serve --bind=0.0.0.0"' >> ~/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+brew install hugo
