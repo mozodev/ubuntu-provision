ce7d6cabd48ec1fbce1fe2e355ed359e7f5e9aba
yeongtaek, Hong <mozodev@users.noreply.github.com>
2021-07-23 11:56:05 +0900

    hugo server alias를 추가합니다.

--- app/hugo.sh
+++ app/hugo.sh
@@ -4,9 +4,9 @@ if [ -f .env ]; then
     export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
 fi
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "NONINTERACTIVE=1 $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
-echo 'alias hs="hugo serve --bind=0.0.0.0"' >> ~/.bashrc
+echo 'alias hs="hugo server -D -b 0.0.0.0"' >> ~/.bash_aliases
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install hugo
