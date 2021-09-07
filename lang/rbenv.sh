2ac92f698e40e4ed43abd9d7af94498dd32a93c7
yeongtaek, Hong <mozodev@users.noreply.github.com>
2021-09-07 15:19:32 +0900

    rbenv 스크립트 오류를 바로잡습니다.

--- lang/rbenv.sh
+++ lang/rbenv.sh
@@ -9,18 +9,19 @@ UBUNTU_USER=${UBUNTU_USER:-ubuntu}
 
 echo [rbenv] install rbenv dependencies
 sudo apt-get install -y build-essential autoconf bison \
-libssl-dev libyaml-dev libreadline6-dev zlib1g-dev \
+libssl-dev libyaml-dev libreadline-dev zlib1g-dev \
 libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev
 
 echo [rbenv] install rbenv
-curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | sudo -u $UBUNTU_USER bash
-sudo -u $UBUNTU_USER cd $UBUNTU_USER/.rbenv && sudo -u $UBUNTU_USER src/configure && sudo -u $UBUNTU_USER make -C src
-sudo -u $UBUNTU_USER echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/$UBUNTU_USER/.bashrc
-sudo -u $UBUNTU_USER echo 'eval "$(rbenv init -)"' >> /home/$UBUNTU_USER/.bashrc
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | su - $UBUNTU_USER -c "bash"
+su - $UBUNTU_USER -c "cd /home/$UBUNTU_USER/.rbenv && src/configure && make -C src"
+su - $UBUNTU_USER -c 'echo '"'"'export PATH=$HOME/.rbenv/bin:$PATH'"'"' >> ~/.bash_profile'
+su - $UBUNTU_USER -c 'echo '"'"'eval "$(rbenv init -)"'"'"' >> ~/.bash_profile'
 
 echo [rbenv] install ruby and bundler gem
 if [ ! -e .rbenv/versions/$RUBY_VERSION ]; then
-  sudo -u $UBUNTU_USER rbenv install $RUBY_VERSION && sudo -u $UBUNTU_USER rbenv global $RUBY_VERSION
-  sudo -u $UBUNTU_USER echo 'gem: --no-ri --no-rdoc --no-document --suggestions' >> /home/$UBUNTU_USER/.gemrc
-  sudo -u $UBUNTU_USER source ~/.bashrc && sudo -u $UBUNTU_USER gem install bundler && sudo -u $UBUNTU_USER ruby -v
+  su - $UBUNTU_USER -c "rbenv install $RUBY_VERSION"
+  su - $UBUNTU_USER -c "rbenv global $RUBY_VERSION"
+  su - $UBUNTU_USER -c "echo 'gem: --no-ri --no-rdoc --no-document --suggestions' >> ~/.gemrc"
+  su - $UBUNTU_USER -c "gem install bundler"
 fi
