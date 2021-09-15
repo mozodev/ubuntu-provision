ccdad7681127fc123ba711053032e7c7049ee52b
yeongtaek, Hong <mozodev@users.noreply.github.com>
2021-09-15 14:00:46 +0900

    node는 그냥 lts만 설치합니다.

--- lang/nodejs.sh
+++ lang/nodejs.sh
@@ -3,16 +3,9 @@ if [ -f /root/.env ]; then
   export $(cat /root/.env | grep -v '#' | awk '/=/ {print $1}')
 fi
 
-NODE_VERSION=${NODE_VERSION:-14}
 UBUNTU_USER=${UBUNTU_USER:-ubuntu}
-ALLOWED_NODE_VERSIONS=('10', '12', '13', '14', '15', '16')
-if [[ "${ALLOWED_NODE_VERSIONS[*]}" =~ "$NODE_VERSION" ]]; then
-  echo [nodejs] add repo for v$NODE_VERSION node, yarn
-  su - $UBUNTU_USER -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash"
-  su - $UBUNTU_USER -c 'echo "export NVM_DIR=$HOME/.nvm" >> .bash_profile'
-  su - $UBUNTU_USER -c 'echo "[ -s $NVM_DIR/nvm.sh ] && \. $NVM_DIR/nvm.sh" >> .bash_profile'
-  su - $UBUNTU_USER -c "nvm install 'lts/*' && nvm alias default 'lts/*' && npm -g i npm yarn"
-else
-  echo "$NODE_VERSION not supported."
-  exit 1
-fi
+echo [nodejs] install node, yarn
+su - $UBUNTU_USER -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash"
+su - $UBUNTU_USER -c 'echo "export NVM_DIR=$HOME/.nvm" >> .bash_profile'
+su - $UBUNTU_USER -c 'echo "[ -s $NVM_DIR/nvm.sh ] && \. $NVM_DIR/nvm.sh" >> .bash_profile'
+su - $UBUNTU_USER -c "nvm install 'lts/*' && nvm alias default 'lts/*' && npm -g i npm yarn"
