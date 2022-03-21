96fd8bffa9c995289e0f41a08d2cee0460e23470
yeongtaek, Hong <mozodev@users.noreply.github.com>
2022-03-21 09:23:43 +0900

    nvm 0.39.0

--- lang/nodejs.sh
+++ lang/nodejs.sh
@@ -5,7 +5,7 @@ fi
 
 UBUNTU_USER=${UBUNTU_USER:-ubuntu}
 echo [nodejs] install node, yarn
-su - $UBUNTU_USER -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash"
+su - $UBUNTU_USER -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash"
 su - $UBUNTU_USER -c 'echo "export NVM_DIR=$HOME/.nvm" >> .bash_profile'
 su - $UBUNTU_USER -c 'echo "[ -s $NVM_DIR/nvm.sh ] && \. $NVM_DIR/nvm.sh" >> .bash_profile'
 su - $UBUNTU_USER -c "nvm install 'lts/*' && nvm alias default 'lts/*' && npm -g i npm yarn"
