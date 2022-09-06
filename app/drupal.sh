958699668ec7599d4557c1120c6ae913cd98c489
yeongtaek, Hong <mozodev@users.noreply.github.com>
2022-09-06 15:50:15 +0900

    app:drupal 사용할 db driver를 지정하면 설치합니다.

--- app/drupal.sh
+++ app/drupal.sh
@@ -7,6 +7,15 @@ fi
 UBUNTU_USER=${UBUNTU_USER:-ubuntu}
 PROJECT_ROOT=${PROJECT_ROOT:-}
 
+# available database: sqlite|mysql|pgsql
+DRUPAL_DB_DRIVER=${DRUPAL_DB_DRIVER:-mysql}
+case "$DRUPAL_DB_DRIVER" in
+  "sqlite") apt-get install -y -qq sqlite3 php-sqlite3 ;;
+  "mysql") apt-get install -y -qq mysql-client php-mysql ;;
+  "pgsql") apt-get install -y -qq postgresql-client php-pgsql ;;
+  *) echo "Unknown database for drupal: ${DRUPAL_DB_DRIVER}" ;;
+esac
+
 echo "[php] install drush launcher"
 wget -O drush.phar https://github.com/drush-ops/drush-launcher/releases/latest/download/drush.phar
 chmod +x drush.phar && sudo mv drush.phar /usr/local/bin/drush
