ea0ee724186457047a38f2ebd8a477a75843de92
yeongtaek, Hong <mozodev@users.noreply.github.com>
2022-09-06 15:50:29 +0900

    cloudflared 설치스크립트를 추가합니다.

---
+++ app/cloudflared.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# check if ubuntu codename exists
+. /etc/os-release
+if [ -z "$VERSION_CODENAME" ]; then;
+ exit -1
+fi
+
+# Add cloudflare gpg key
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+
+# Add this repo to your apt repositories
+echo "deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared $VERSION_CODENAME main" | sudo tee /etc/apt/sources.list.d/cloudflared.list
+
+# install cloudflared
+sudo apt-get update && sudo apt-get install cloudflared
