be740a54b3968c740f3f265d0e64a52702441987
yeongtaek, Hong <mozodev@users.noreply.github.com>
2023-08-16 13:21:41 +0900

    wp php-imagick

--- app/wp.sh
+++ app/wp.sh
@@ -7,3 +7,7 @@ fi
 echo "[php] install wp-cli globally"
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar && sudo mv wp-cli.phar /usr/local/bin/wp
+
+echo "[php] install imagick"
+PHP_VERSION=${PHP_VERSION:-8.1}
+apt-get -y -qq install php${PHP_VERSION}-imagick
